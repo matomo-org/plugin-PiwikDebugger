4e9aaef3bea7626d170b388aa3950bd375f4fd73
Thomas Steur <thomas.steur@googlemail.com>
2014-08-12 18:25:53 +0200

    refs #4 #5 added two simple menu entries for editing files and executing queries as well as two controller actions

---
+++ Controller.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+namespace Piwik\Plugins\PiwikDebugger;
+
+use Piwik\View;
+
+/**
+ *
+ */
+class Controller extends \Piwik\Plugin\ControllerAdmin
+{
+
+    public function queryDb()
+    {
+        $view = new View('@PiwikDebugger/queryDb.twig');
+        $this->setBasicVariablesView($view);
+        $view->answerToLife = '42';
+
+        return $view->render();
+    }
+
+    public function editFiles()
+    {
+        $view = new View('@PiwikDebugger/editFiles.twig');
+        $this->setBasicVariablesView($view);
+        $view->answerToLife = '42';
+
+        return $view->render();
+    }
+}
